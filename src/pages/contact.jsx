071fad3a048775234453a7810d68c6a249009835
Aryan Agarwal <aryan.agarwal@rootquotient.com>
2026-01-27 18:31:07 +0530

    React router changes

---
+++ src/pages/contact.jsx
@@ -0,0 +1,10 @@
+function Contact() {
+  return (
+    <div className="page">
+      <h1>Contact Page</h1>
+      <p>You can add a form here later.</p>
+    </div>
+  );
+}
+
+export default Contact;
---
+++ src/pages/home.jsx
@@ -0,0 +1,11 @@
+function Home() {
+  return (
+      <div className="page">
+      <h1>Home Page</h1>
+      <p>Welcome to the basic React Router app.</p>
+      </div>
+  );
+  }
+
+export default Home;
+  
